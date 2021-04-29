3881ad942c952882f99627e7d0dff96b7bac9912
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-04-29 10:09:30 +0530

    Update __main__.py

--- SheebaQueen/__main__.py
+++ SheebaQueen/__main__.py
@@ -84,7 +84,7 @@ buttons = [
             text="➕️ Add Sheeba The Queen To Your Group ➕️", url="t.me/Sheeba_Queenbot?startgroup=true"),
     ],
     [
-        InlineKeyboardButton(text="About", callback_data="Sheeba_"),
+        InlineKeyboardButton(text="About", callback_data="sheeba_"),
         InlineKeyboardButton(
             text="Support", url=f"https://t.me/{SUPPORT_CHAT}"
         ),
